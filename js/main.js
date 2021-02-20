50873cb9d8b137e25e36cd7f82415aea87c4bb00
Agustín Lozano Blua <73617542+LordPekers@users.noreply.github.com>
2021-02-20 14:27:13 -0300

    Add files
    Este commit representa la primera versión del programa, incluyendo:
      • main.js
      • main. css
      • index.html

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+const API_KEY = '32531d4ff9434067eca3f2704fd8ab5f';
+const fetchData = position => {
+    const {latitude,longitude} = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherdata(data))
+
+}
+
+const setWeatherdata = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    }); 
+
+    cleanup(); 
+}
+
+const cleanup = () => {
+    let container = document.getElementById('container');
+    let loeader = document.getElementById('loader');
+
+    loeader.style.display = 'none';
+    container.style.display = 'flex';
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
